b289bb42a7b1c292e13eebb2725db0857242b112
hendrysitorus <hendrysitorus20@gmail.com>
2023-05-18 14:22:05 +0700

    update input nama & logic

--- main.go
+++ main.go
@@ -4,12 +4,38 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("we have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	//untuk mencari tipe data dengan %T
+	fmt.Printf(" conferenceName is %T, conferenceTickets is %T, remainingTickets is %T \n", conferenceName, conferenceTickets, remainingTickets)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	// aks user fot their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank You %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v ticket remaining for %v\n", remainingTickets, conferenceName)
 
 }
