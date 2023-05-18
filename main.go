310f3c74c3d7667cf1687b0b3fa467bbfd88c2e3
hendrysitorus <hendrysitorus20@gmail.com>
2023-05-18 10:44:30 +0700

    update variable & constantas in go

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to out conference booking application")
+
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("we have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
+
 }
